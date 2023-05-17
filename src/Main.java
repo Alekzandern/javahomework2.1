a315ca6b78b0d3b4cb06e82ecc7a9c7a755b53cc
Alexzander <Alex.Zander54.ru>
2023-05-17 22:20:52 +0000

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        int cost = 64000; // стоимость билета
+        int bonus = 20; // количество рублей для одной бонусной мили
+        System.out.println(cost / bonus); //количество бонусных милей
+        System.out.println("Bye");
+    }
+}
